9349e8c6b4511a334dc8b4f95ccfede69ac3096f
kenneth <kenneth@smartwave.ph>
2015-04-14 18:15:13 +0800

    asdasd

---
+++ BasicElements/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicElements
+//
+//  Created by smartwave on 4/14/15.
+//  Copyright (c) 2015 AppVenture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BasicElements/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasicElements
+//
+//  Created by smartwave on 4/14/15.
+//  Copyright (c) 2015 AppVenture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
